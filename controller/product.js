5edfed863028cf614e35f9f963d7baa993760126
Behruz11234 <komilovbehruz5500@gmail.com>
2026-05-08 16:30:58 +0500

    init

--- controller/product.js
+++ controller/product.js
@@ -3,126 +3,143 @@ const { read_file, write_file } = require("../fs/file-manager")
 
 
 // 1 get_all_products
-  const getALLProducts = async (req, res) => {
-    try {
-const products = read_file("prducts.json")
+const getALLProducts = async (req, res) => {
+  try {
+    const products = read_file("prducts.json")
 
-res.status(200).json(products)
-    } catch (err) {
-      return res.status(500).json({
-        massage: err.massage
-      })
-    }
+    res.status(200).json(products)
+  } catch (err) {
+    return res.status(500).json({
+      massage: err.massage
+    })
   }
+}
 
-  // 2 get_one_products
-  const getOneProduct = async (req, res) => {
-    try {
-      const { id } = req.params
-      const products = read_file("prducts.json")
+// 2 get_one_products
+const getOneProduct = async (req, res) => {
+  try {
+    const { id } = req.params
+    const products = read_file("prducts.json")
 
-      const foundProducts = products.find((pro) => pro.id === id)
+    const foundProducts = products.find((pro) => pro.id === id)
 
-     if(!foundProducts) {
+    if (!foundProducts) {
       return res.status(400).json({
         massage: "product not found"
       })
-     }
-
-     res.statuso(200).json(foundProducts)
-    } catch (err) {
-      return res.status(500).json({
-        massage: err.massage
-      })
     }
+
+    res.statuso(200).json(foundProducts)
+  } catch (err) {
+    return res.status(500).json({
+      massage: err.massage
+    })
   }
+}
 
 
-  //  3
-  const addProduct = async (req, res) => {
-    try {
+//  3
+const addProduct = async (req, res) => {
+  try {
     const { title, price, quantity } = req.body
 
-      const products = read_file("prducts.json")
+    const products = read_file("prducts.json")
 
-      products.push({
-        id: uuid.v4(),
-        title,
-        price,
-        quantity
-      })
+    products.push({
+      id: uuid.v4(),
+      title,
+      price,
+      quantity
+    })
 
-      write_file("products.json", products)
+    write_file("products.json", products)
 
-      res.status(201).json({
-        massage: "Added new products"
-      })
+    res.status(201).json({
+      massage: "Added new products"
+    })
 
-    } catch (err) {
-      return res.status(500).json({
-        massage: err.massage
-      })
+  } catch (err) {
+    return res.status(500).json({
+      massage: err.massage
+    })
 
 
 
-    }
   }
+}
 
 
-  //  4
-  const updateProduct = async (req, res) => {
-    try {
+//  4
+const updateProduct = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { title, price, quantity } = req.body;
+    const products = read_file("products.json");
 
-     const { id } = req.params
-      const products = read_file("prducts.json")
+    const foundedProduct = products.find((pro) => pro.id === id);
 
-      const foundProducts = products.find((pro) => pro.id === id)
-
-     if(!foundProducts) {
-      return res.status(400).json({
-        massage: "product not found"
-      })
-     }
-
-     products.array.forEach(element => {
-      
-     });
-    } catch (err) {
-      return res.status(500).json({
-        massage: err.massage
-      })
+    if (!foundedProduct) {
+      return res.status(404).json({
+        message: "product not found",
+      });
     }
+
+    products.forEach((product) => {
+      if (product.id === id) {
+        product.title = title ? title : product.title;
+        product.price = price ? price : product.price;
+        product.quantity = quantity ? quantity : product.quantity;
+      }
+    });
+
+    write_file("products.json", products);
+
+    res.status(200).json({
+      message: "Updated product",
+    });
+  } catch (err) {
+    return res.status(500).json({
+      message: err.message,
+    });
   }
+};
 
 
   //  5
-  const deleteProduct = async (req, res) => {
-    try {
+const deleteProducts = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const products = read_file("products.json");
 
-            const { id } = req.params
-      const products = read_file("prducts.json")
+    const foundedProduct = products.find((pro) => pro.id === id);
 
-      const foundProducts = products.find((pro) => pro.id === id)
-
-     if(!foundProducts) {
-      return res.status(400).json({
-        massage: "product not found"
-      })
-     }
-
-    } catch (err) {
-      return res.status(500).json({
-        massage: err.massage
-      })
-
-
+    if (!foundedProduct) {
+      return res.status(404).json({
+        message: "product not found",
+      });
     }
-  }
 
+    products.forEach((product, idx) => {
+      if (product.id === id) {
+        products.splice(idx, 1);
+      }
+    });
+
+    write_file("products.json", products);
+
+    res.status(200).json({
+      message: "Deleted product",
+    });
+  } catch (err) {
+    return res.status(500).json({
+      message: err.message,
+    });
+  }
+};
   module.exports = {
-   getALLProducts,
+    getALLProducts,
     addProduct,
     getOneProduct,
     updateProduct,
-    deleteProduct
+    deleteProducts
   }
